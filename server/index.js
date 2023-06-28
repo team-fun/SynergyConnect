beb08fd92303f89f0e8cbd7572965c947d30d802
Jovan Stosic <jovanstosic012@gmail.com>
2023-06-28 14:20:19 -0500

    Fixed the name

--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
-const { db } = require('./db')
-const PORT = process.env.PORT || 8080
-const app = require('./app')
-const seed = require('../script/seed');
+const { db } = require("./db");
+const PORT = process.env.PORT || 8080;
+const app = require("./app");
+const seed = require("../script/seed");
 
 const init = async () => {
   try {
-    if(process.env.SEED === 'true'){
+    if (process.env.SEED === "true") {
       await seed();
-    }
-    else {
-      await db.sync()
+    } else {
+      await db.sync();
     }
     // start listening (and create a 'server' object representing our server)
-    app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
+    app.listen(PORT, () =>
+      console.log(`🛸🛸🛸🛸🛸 Mixing it up on http://localhost:3000 🔪🔪🔪🔪`)
+    );
   } catch (ex) {
-    console.log(ex)
+    console.log(ex);
   }
-}
+};
 
-init()
+init();
