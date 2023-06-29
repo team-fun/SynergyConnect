9f4050144088ab32bb26109e33341ca178d18de1
Jovan Stosic <jovanstosic012@gmail.com>
2023-06-29 13:53:07 -0500

    Starting the basics for the chat room to create from home

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
-import AuthForm from '../features/auth/AuthForm';
-import Home from '../features/home/Home';
-import { me } from './store';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import AuthForm from "../features/auth/AuthForm";
+import Home from "../features/home/Home";
+import { me } from "./store";
 
 /**
  * COMPONENT
@@ -23,6 +23,7 @@ const AppRoutes = () => {
         <Routes>
           <Route path="/*" element={<Home />} />
           <Route to="/home" element={<Home />} />
+          <Route to="/chat" element={<Home />} />
         </Routes>
       ) : (
         <Routes>
--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3001/");
 
 /**
  * COMPONENT
  */
 const Home = (props) => {
   const username = useSelector((state) => state.auth.me.username);
+  const [room, setRoom] = useState("");
+
+  const joinRoom = () => {};
 
   return (
     <div>
       <h3>Welcome, {username}</h3>
+      <button onClick={joinRoom}>Create A Room</button>
     </div>
   );
 };
