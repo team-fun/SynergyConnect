9f842bd5553c490249d2e99b7a5bd1e4acbd8abb
Jovan Stosic <jovanstosic012@gmail.com>
2023-06-29 16:27:53 -0500

    Added the chats to store

--- client/app/store.js
+++ client/app/store.js
@@ -1,14 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
-import authReducer from '../features/auth/authSlice';
-import createRoomFormSlice from '../features/home/createRoomFormSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import authReducer from "../features/auth/authSlice";
+import createRoomFormSlice from "../features/home/createRoomFormSlice";
+import AllChatsSlice from "../features/home/AllChatsSlice";
 
 const store = configureStore({
-  reducer: { auth: authReducer, 
-             createRoom: createRoomFormSlice            
+  reducer: {
+    auth: authReducer,
+    createRoom: createRoomFormSlice,
+    chats: AllChatsSlice,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
-export * from '../features/auth/authSlice';
+export * from "../features/auth/authSlice";
