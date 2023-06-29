eb6c8b314112bf1f498ca2ab291c6a8d0d8d4507
Daniel Rodriguez <drodrigo95@hotmail.com>
2023-06-29 14:33:39 -0500

    created the thunk and slice for createRoom

--- client/app/store.js
+++ client/app/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import authReducer from '../features/auth/authSlice';
+import createRoomFormSlice from '../features/home/createRoomFormSlice';
 
 const store = configureStore({
-  reducer: { auth: authReducer },
+  reducer: { auth: authReducer, 
+             createRoom: createRoomFormSlice            
+  },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
---
+++ client/features/home/createRoomFormSlice.js
@@ -0,0 +1,41 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = []
+
+export const fetchCreateRoomAsync = createAsyncThunk(
+    'createRoomAsync',
+    async ({ name, code, description, isPublic }) => {
+        try {
+            const { data } = await axios.get('/api/createroom', {
+                name: name,
+                code: code,
+                description: description,
+                isPublic: isPublic,
+            })
+            console.log(data);
+            return data
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+)
+
+const createRoomSlice =  createSlice({
+    name: 'createRoom',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+        .addCase(fetchCreateRoomAsync. fulfilled, (state, action) => {
+            return action.payload
+        })
+    }, 
+})
+
+export const selectCreatedRoom = (state) => {
+    return state.createRoom
+}
+
+export default createRoomSlice.reducer
