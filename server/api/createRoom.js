ed008844ea05cc43e40d63d7d37baf6492bc949a
Jovan Stosic <jovanstosic012@gmail.com>
2023-06-29 14:55:23 -0500

    Created the basic route

---
+++ server/api/createRoom.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const Chat = require("../db/models/Chat");
+
+router.post("/", async (req, res, next) => {
+  try {
+    console.log(req.body);
+    const newChat = await Chat.create(req.body);
+    res.send(newChat);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
