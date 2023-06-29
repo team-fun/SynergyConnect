23ffb5133012656c47390f86253c9e59b362357b
Adam Green <greenap89@gmail.com>
2023-06-29 17:26:49 -0400

    admin functionality created

---
+++ server/api/admin.js
@@ -0,0 +1,48 @@
+const router = require('express').Router()
+const { models: { User }} = require('../db')
+module.exports = router
+
+router.get('/', async (req, res, next) => {
+    try {
+      const users = await User.findAll({
+        attributes: ['id', 'username', 'isAdmin', 'firstName', 'lastName', 'email', 'interests']
+      })
+      res.json(users)
+    } catch (err) {
+      next(err)
+    }
+  })
+
+  router.get('/:id', async (req, res, next) => {
+    try {
+		const user = await User.findOne({
+			where: { id: req.params.id },
+		});
+    res.json(user)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete("/:id", async (req, res) => {
+	const { id } = req.params;
+
+	try {
+		const data = await User.findByPk(id);
+		data.destroy();
+		res.send(data);
+	} catch (err) {
+		res.sendStatus(500);
+	}
+});
+
+router.put("/:id", async (req, res, next) => {
+	try {
+		const user = await User.findOne({
+			where: { id: req.params.id },
+		});
+		res.send(await user.update(req.body));
+	} catch (error) {
+		next(error);
+	}
+});
--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,31 @@
 const router = require('express').Router()
+const { models: { User }} = require('../db')
 module.exports = router
 
+const requireToken = async (req, res, next) => {
+	try {
+		const token = req.headers.authorization
+		const user = await User.findByToken(token)
+        req.user = user
+		next()
+	} catch (e) {
+		next(e)
+	}
+}
+
+const isAdminCheck = (req, res, next) => {
+	requireToken(req, res, next, () => { 
+    if (!req.user.isAdmin) {
+      return res.status(403).send('Unauthorized: You do not have access to this page')
+    } else {
+      next()
+    }
+  })
+}
+
 router.use('/users', require('./users'))
+router.use('/admin', isAdminCheck, require('./admin'))
+
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
