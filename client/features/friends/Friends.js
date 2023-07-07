e18400c5a58f5aadcdc0b98b8fed8734bf583cc9
Jovan Stosic <jovanstosic012@gmail.com>
2023-07-07 11:04:50 -0500

    Settng up the component and adding the search bar in

--- client/features/friends/Friends.js
+++ client/features/friends/Friends.js
@@ -1,9 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { sendFriendRequest } from "../home/AllFriendsSlice";
+import {
+  fetchAllNonFriends,
+  selectNonFriends,
+} from "../home/AllNonFriendsSlice";
+import SearchBox from "../seachbar/SearchBar";
 
 const Friends = () => {
+  const id = useSelector((state) => state.auth.me.id);
+  const [friendListChange, setfriendListChange] = useState(false);
+  const nonFriends = useSelector(selectNonFriends) || [];
+  const [filter, setFilter] = useState([]);
+  const [search, setSearch] = useState("");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAllNonFriends({ id }));
+  }, [dispatch, friendListChange]);
+
+  const handleFriendListChange = () => {
+    setfriendListChange(!friendListChange);
+  };
+
+  const handleSendRequest = (friendID) => {
+    dispatch(
+      sendFriendRequest({
+        loggedInUserId: id,
+        otherFriendId: friendID,
+      })
+    );
+
+    setTimeout(() => {
+      handleFriendListChange();
+    }, 1000);
+  };
+
+  const onSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
+
   return (
     <div>
-      <h1>Hi</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <h1>Non Friends</h1>
+      {nonFriends.length === undefined || nonFriends?.length == 0 ? (
+        <div>All Users are friends</div>
+      ) : (
+        <div>
+          {nonFriends?.map((nonFriend, i) => (
+            <div key={i}>
+              {nonFriend.username}{" "}
+              <button onClick={() => handleSendRequest(nonFriend.id)}>+</button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
