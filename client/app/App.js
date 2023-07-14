6af479f9a20726aa9984f185774322e3cb6f4e02
Adam Green <greenap89@gmail.com>
2023-07-14 11:15:45 -0400

    fixed cursor overlay

--- client/app/App.js
+++ client/app/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "tailwindcss/tailwind.css";
 import Navbar from "../features/navbar/Navbar";
 import AppRoutes from "./AppRoutes";
-import CursorOverlay from "../features/chat/WhiteBoard/CursorOverlay/CursorOverlay";
 import { Provider } from "react-redux";
 import store from "./store";
 import { connectWithSocketServer } from "../features/chat/WhiteBoard/socketConn/socketConn";
@@ -22,7 +21,6 @@ const App = () => {
           <Navbar />
           <AppRoutes />
         </div>
-        <CursorOverlay />
         <Footer />
       </Provider>
     </div>
