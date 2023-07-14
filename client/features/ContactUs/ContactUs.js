db3dc4473b4cd8b2749e6cb33be9f2a4803bfbf3
Olorunfemi <Femibam1@gmail.com>
2023-07-14 17:31:33 -0400

    communityGuideLines

--- client/features/ContactUs/ContactUs.js
+++ client/features/ContactUs/ContactUs.js
@@ -7,7 +7,7 @@ import { ContactData } from "./ContactData";
 const ContactUs = () => {
   return (
     <div>
-      <h3>Synergy Connect is brought to you by the following</h3>
+      <h3 className="ContactUsHeader">Synergy Connect is brought to you by our outstanding team!</h3>
       <div className="container my-12 mx-auto px-4 md:px-12">
         <div className="flex flex-wrap justify-around mx-1 lg:-mx-4">
           {ContactData.map((singleContact, i) => (
