b6d640361adac57118fc3e5607c7f62dceba2c55
Jovan Stosic <jovanstosic012@gmail.com>
2023-07-14 20:33:37 -0500

    forgot the whiteboard

--- client/features/chat/WhiteBoard/socketConn/socketConn.js
+++ client/features/chat/WhiteBoard/socketConn/socketConn.js
@@ -1,37 +1,37 @@
-import io from 'socket.io-client';
-import { setElements, updateElement } from '../whiteboardSlice';
-import store  from "../../../../app/store";
+import io from "socket.io-client";
+import { setElements, updateElement } from "../whiteboardSlice";
+import store from "../../../../app/store";
 
 let socket;
 
 export const connectWithSocketServer = () => {
-    socket = io('http://localhost:3001');
-    
-    socket.on('connect', () => {
-        console.log('connected to socket.io server');
-    });
+  socket = io("https://synergy-connect.onrender.com");
 
-    socket.on('whiteboard-state', (elements) => {
-        store.dispatch(setElements(elements))
-    });
+  socket.on("connect", () => {
+    console.log("connected to socket.io server");
+  });
 
-    socket.on('element-update', (elementData) => {
-        store.dispatch(updateElement(elementData))
-    });
+  socket.on("whiteboard-state", (elements) => {
+    store.dispatch(setElements(elements));
+  });
 
-    socket.on('whiteboard-clear', () => {
-        store.dispatch(setElements([]))
-    });
+  socket.on("element-update", (elementData) => {
+    store.dispatch(updateElement(elementData));
+  });
+
+  socket.on("whiteboard-clear", () => {
+    store.dispatch(setElements([]));
+  });
 };
 
 export const emitElementUpdate = (elementData) => {
-    socket.emit('element-update', elementData);
+  socket.emit("element-update", elementData);
 };
 
 export const emitClearWhiteboard = () => {
-    socket.emit('whiteboard-clear')
+  socket.emit("whiteboard-clear");
 };
 
 export const emitCursorPosition = (cursorData) => {
-    socket.emit('cursor-position', cursorData);
+  socket.emit("cursor-position", cursorData);
 };
