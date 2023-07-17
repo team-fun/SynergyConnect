d4f0259a8cee6d5f88e90f3c4f9e580864b81c20
Jovan Stosic <jovanstosic012@gmail.com>
2023-07-16 19:14:20 -0500

    removed the console logs

--- server/api/createRoom.js
+++ server/api/createRoom.js
@@ -4,14 +4,12 @@ const Chat = require("../db/models/Chat");
 router.post("/", async (req, res, next) => {
   try {
     const { name, code, description, isPublic } = req.body;
-    console.log(name, code, description, isPublic);
     const newChat = await Chat.create({
       name: name,
       code: code,
       description: description,
       public: isPublic,
     });
-    console.log("NEWWWWWWWWWW", newChat);
     res.send(newChat);
   } catch (err) {
     next(err);
